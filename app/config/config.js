b2d148f490400cbd46aa95a103cea59dbf8143a7
Greg S <greg@evias.be>
2017-03-08 15:58:46 +0100

    read MONGODB_URI and NIS_* config from heroku environment variables

--- app/config/config.js
+++ app/config/config.js
@@ -1,8 +1,8 @@
 const config = {
-	port: 8081,
-	mongodb: 'mongodb://localhost/explorer',
-	nisHost: '127.0.0.1',
-	nisPort: '7890',
+	port: process.env.PORT || 8081,
+	mongodb: process.env.MONGODB_URI || 'mongodb://localhost/explorer',
+	nisHost: process.env.NIS_HOST || '127.0.0.1',
+	nisPort: process.env.NIS_PORT || '7890',
 	supernodeHost: 'https://supernodes.nem.io',
 	supernodePayoutAccount: 'NCPAYOUTH2BGEGT3Q7K75PV27QKMVNN2IZRVZWMD',
 	nisInitStartBlock: 0 //default 0
